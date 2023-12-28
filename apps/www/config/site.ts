a6fbb8ed566cd3b60012a5fc3fc9be4e4cd70b34
Mukesh Singh <camukeshkumarsingh@gmail.com>
2023-12-29 01:52:44 +0530

    feat: repo setup done

---
+++ apps/www/config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "MKSingh/ui",
+  url: "https://ui.mksingh.in",
+  ogImage: "https://ui.mksingh.in/og.jpg",
+  description:
+    "Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.",
+  links: {
+    twitter: "https://twitter.com/ca_mksingh",
+    twitterShadcn: "https://twitter.com/shadcn",
+    github: "https://github.com/mksingh-ui/ui",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
